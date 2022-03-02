347b381d79d6c7a57f514563a5ba9f319fee5fc8
Razi Ahmad <mail2razi4u@gmail.com>
2022-03-02 11:44:47 +0530

    testing of tutors

--- tutorchamps/settings.py
+++ tutorchamps/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'tutorchamps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+       'NAME': config('NAME'),
+       'USER': config('USER'),
+       'PASSWORD': config('PASSWORD'),
+       'HOST': config("HOST"),
+       'PORT': config("PORT"),
+        
     }
 }
 
