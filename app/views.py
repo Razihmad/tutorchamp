ef31ffb885d2ad6ca695f92b2a53952f1f9eabef
Razi Ahmad <mail2razi4u@gmail.com>
2022-04-12 01:10:56 +0530

    email issue resolved

--- app/views.py
+++ app/views.py
@@ -48,7 +48,7 @@ def password_reset_request(request):
                     'protocol': 'http',
                 }
                 email = render_to_string(email_template_name, c)
-                connection = mail.get_connection(backend='django.core.mail.backends.smtp.EmailBackend',host='smtp.hostinger.com',
+                connection = mail.get_connection(backend='django.core.mail.backends.smtp.EmailBackend',host='smtp.gmail.com',
                                          use_tls=True,port=587,username='admin@tutorchamps.com',password=config('adminPassword'))
                 connection.open()
                 try:
@@ -623,7 +623,7 @@ def tutor_register(request):
                 'user':name
             }
             email_msg = render_to_string('tutor_email.txt',c)
-            connection = mail.get_connection(backend='django.core.mail.backends.smtp.EmailBackend',host='smtp.hostinger.com',name="TutorChamps",
+            connection = mail.get_connection(backend='django.core.mail.backends.smtp.EmailBackend',host='smtp.gmail.com',name="TutorChamps",
                                              use_tls=True,port=587,username='tutors@tutorchamps.com',password=config('tutorPassword'))
             connection.open()
             email = EmailMessage(subject='Welcome to TutorChamps || Complete the test',body=email_msg,from_email='TutorChamps Tutors Support <tutors@tutorchamps.com>',to=[email])
@@ -744,7 +744,7 @@ def interest(request):
                 'order_id':order_id,
             }
         email_msg = render_to_string('tutor_email.txt',c)
-        connection = mail.get_connection(backend='django.core.mail.backends.smtp.EmailBackend',host='smtp.hostinger.com',name="TutorChamps",
+        connection = mail.get_connection(backend='django.core.mail.backends.smtp.EmailBackend',host='smtp.gmail.com',name="TutorChamps",
                                           use_tls=True,port=587,username='tutors@tutorchamps.com',password=config('tutorPassword'))
         connection.open()
         email = EmailMessage(subject='tutor interest has com',body=email_msg,from_email='TutorChamps Tutors Support <tutors@tutorchamps.com>',to=['adm.tutorchamps@gmail.com'])
--- tutorchamps/settings.py
+++ tutorchamps/settings.py
@@ -142,7 +142,7 @@ USE_L10N = True
 
 USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.hostinger.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
